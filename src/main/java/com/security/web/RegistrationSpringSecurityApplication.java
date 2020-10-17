326eaf090b785a346016efb76ef4ad61584f883f
Regis Maximo <regishallowed@gmail.com>
2020-10-17 12:49:15 -0300

    first commit

---
+++ src/main/java/com/security/web/RegistrationSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSpringSecurityApplication.class, args);
+	}
+
+}
